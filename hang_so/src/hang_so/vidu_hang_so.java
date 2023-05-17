0ea26130aeda8207d9b9a4d1fecc803d9ca934c1
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-05-17 15:18:56 +0700

    Hằng số tức là chỉ gán giá trị khi khai báo không được thay đổi giá trị về sau. cú pháp là thêm từ khoá final trước kiểu dữ liệu

---
+++ hang_so/src/hang_so/vidu_hang_so.java
@@ -0,0 +1,11 @@
+package hang_so;
+
+public class vidu_hang_so {
+
+	public static void main(String[] args) {
+		final int x = 17;
+		System.out.println("Giá trị của x là: " + x);
+		//x = 20; nếu thay đổi giá trị của final sẽ bị lỗi
+	}
+
+}
