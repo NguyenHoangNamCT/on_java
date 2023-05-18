d10552a7ffcb6c855c848c75f24fa6f45824a5e0
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2023-05-18 17:59:57 +0700

    Phuong thuc tostring của đối tượng trong java. có thể in trực tiếp tên đối tượng sẽ xuất hiện chuổi trong phương thức tostring không cần gọi phương thức tostring

---
+++ phuong_thuc_to_string_cua_doi_tuong/src/phuong_thuc_to_string_cua_doi_tuong/MyMain.java
@@ -0,0 +1,10 @@
+package phuong_thuc_to_string_cua_doi_tuong;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		MyDate d1 = new MyDate(17, 10, 2001);
+		System.out.print(d1);
+	}
+
+}
